db44aa2dce3aa8aaa1f0975c5ca06c93a9faead1
TraBee <tra8brown@gmail.com>
2021-08-01 12:03:07 -0600

    added env data

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-const homeRoutes = require('.home-routes.js');
+const homeRoutes = require('../controllers/home-routes.js');
 
 router.use('/', homeRoutes);
 
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const path = require('path'); //css path
+const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+
+const app = express();
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); //css route
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
